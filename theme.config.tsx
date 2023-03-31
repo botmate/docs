f41bf2202c82fae66c24e89c3967414b8e31120d
xencodes <xencodes@icloud.com>
2023-03-31 08:54:22 +0530

    add favicon

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,6 +5,20 @@ import { Logo } from './components/logo';
 
 const config: DocsThemeConfig = {
 	logo: <Logo />,
+	head: (
+		<>
+			<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+			<meta property='og:title' content='BotMate Docs' />
+			<meta property='og:description' content='Documentation for BotMate' />
+			<meta property='og:type' content='website' />
+			<meta property='og:url' content='https://docs.botmate.io' />
+			<meta
+				property='og:image'
+				content='https://docs.botmate.io/og-image.png'
+			/>
+			<link rel='icon' href='/favicon.ico' />
+		</>
+	),
 	project: {
 		link: 'https://github.com/botmate/botmate',
 	},
